b061205450cbfe26c6626060290e1715f8b1d5f1
Brijesh Rajput <rbk4112001@gmail.com>
2023-07-17 19:28:08 +0530

    initial commit

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ const PORT = 5000;
 
 const app = express();
 app.use(express.json({ limit: "1mb" }));
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(compression()); // compression middleware
 
